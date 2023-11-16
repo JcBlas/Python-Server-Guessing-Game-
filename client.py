f2d6e024b51e2eafd94ba2523ebd4ea0bf1afb58
JcBlas <121151389+JcBlas@users.noreply.github.com>
2023-11-16 09:28:08 +0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,26 @@
+# client.py
+
+import socket
+
+def start_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = '127.0.0.1'
+    port = 12345
+
+    client_socket.connect((host, port))
+    print("Connected to server")
+
+    while True:
+        guess = input("Guess the number (between 1 and 100): ")
+        client_socket.send(guess.encode())
+
+        response = client_socket.recv(1024).decode()
+        print(response)
+
+        if "Congratulations" in response:
+            break
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    start_client()
